9c66104ed692617101bf33bc403d3dfdf7713ddb
a.chertkov <a.chertkov@brandquad.ru>
2019-05-03 11:09:17 +0300

    Fix

--- src/js/config.js
+++ src/js/config.js
@@ -1,8 +1,8 @@
 let configs = {
-  BACKEND_URL: 'http://127.0.0.1:8080',
+  BACKEND_URL: 'http://127.0.0.1:8000',
   AUTH_URL: 'http://127.0.0.1:9325',
-  BACKEND_WS: 'ws://127.0.0.1:8080/api/ws',
-  MEDIA_URL: 'http://127.0.0.1:8080'
+  BACKEND_WS: 'ws://127.0.0.1:8000/api/ws',
+  MEDIA_URL: 'http://127.0.0.1:8000'
 };
 
 export default configs;
